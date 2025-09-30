d773d40f1ec5ff378adf7badc4d39e9dc255c42e
wyna3209 <skdnjsduq@naver.com>
2025-09-30 23:49:09 +0900

    커밋 메시지 예시: 타입 오류 수정: Notice 필드 및 태그 비교 로직 일치화

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,15 +21,7 @@ const router = createRouter({
         requiresApproval: true
       }
     },
-    {
-      path: '/collection',
-      name: 'collection',
-      component: () => import('../views/CollectionView.vue'),
-      meta: {
-        requiresAuth: true,
-        requiresApproval: true
-      }
-    },
+    
     {
       path: '/feedback',
       name: 'feedback',
